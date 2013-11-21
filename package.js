e405245c45bb271032e51f2cd32896beac8cc60f
Rob Wilburn <rob@mathist.net>
2013-11-21 11:27:00 -0700

    Simplified to just angular

--- package.js
+++ package.js
@@ -3,14 +3,5 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
-  Npm.depends({
-    connect: '2.8.8'
-  });
-
-  api.use('webapp', ['server']);
-
   api.add_files('angular.js', 'client');
-  api.add_files('client.js', 'client');
- 
-  api.add_files('server.js', 'server');
 });
